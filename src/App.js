51923847972a1bd6fc1529406dc3f6f2f1b77739
mohit-Byte-Roamer <mohit878421@gmail.com>
2024-04-28 22:59:46 +0530

    correction in code

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <SignIn />
-      {/* <AppLayout /> */}
+      {/* <SignIn /> */}
+      <AppLayout />
     </div>
   );
 }
